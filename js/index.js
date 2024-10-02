c3a12f4e894c184d81cebeeaaaea8ba5798dae9a
UgoMonnier <ugomonnier@live.fr>
2024-10-02 16:44:48 +0200

    exo 3

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ function moy(array) {
   array.forEach(element => {
     sum += element;
   });
-  return Math.round( sum / array.length);
+  return Math.round(sum / array.length);
 }
 
 // Attribution du niveau en fonction de la moyenne
@@ -65,33 +65,92 @@ tab.forEach(element => {
 
 // let  mot1 = prompt("choisissez un mot : ").trim().toUpperCase();
 // let  mot2 = prompt("choisissez un mot : ").trim().toUpperCase();
+// let tab_mot1 = mot1.split("");
+// let tab_mot2 = mot2.split("");
 
-// // expression Ternaires :
-// mot1 === mot2 ? console.log("Ces deux mots sont identiques") : console.log("Ces deux mots ne sont pas identiques");
+// function trimCopy(mot) {
+//   let tab_mot = mot.split("");
+//   let new_tab = [];
+//   for (let i = 0; i < tab_mot.length; i++) {
+//     if (tab_mot[i] !== " ") {
+//       new_tab.push(tab_mot[i]);
+//     }
+//   }
+//   return new_tab;
+// }
 
-// trouver des mots palindromes
-function arePalindrome(mot1, mot2) {
-  let tab_mot1 = mot1.split('');
-  let tab_mot2 = mot2.split('');
-  if (tab_mot1.length != tab_mot1.length) 
-    {return "Les mots ne sont pas des palindrome";} 
-  let i = 0;
-  let j = tab_mot1.length - 1;
+// function areIdentique(mot1, mot2) {
+//   let tab_mot1 = trimCopy(mot1);
+//   let tab_mot2 = trimCopy(mot2);
 
-  while( i < tab_mot1.length ){    
-    if(tab_mot1[i] === tab_mot2[j])
-    {
-      i++;
-      j--;
-    }else{
-      return "Les mots ne sont pas des palindrome";
+//   if (tab_mot1.length !== tab_mot2.length) {
+//     return "Les mots ne sont pas identique";
+//   }
+
+//   let i = 0;
+//   while (i < tab_mot1.length) {
+//     if (tab_mot1[i] === tab_mot2[i]) {
+//       i++;
+//     } else {
+//       return "Les mots ne sont pas identiques";
+//     }
+//   }
+//   return "Les mots sont identiques";
+// }
+
+// console.log(areIdentique(mot1, mot2));
+
+// // trouver des mots palindromes
+// function arePalindrome(mot1, mot2) {
+//   let tab_mot1 = mot1.split("");
+//   let tab_mot2 = mot2.split("");
+//   if (tab_mot1.length != tab_mot1.length) {
+//     return "Les mots ne sont pas des palindrome";
+//   }
+//   let i = 0;
+//   let j = tab_mot1.length - 1;
+
+//   while (i < tab_mot1.length) {
+//     if (tab_mot1[i] === tab_mot2[j]) {
+//       i++;
+//       j--;
+//     } else {
+//       return "Les mots ne sont pas des palindrome";
+//     }
+//   }
+//   return "Les mots sont des palindrome";
+// }
+
+// verification mot de passe
+
+let mot_de_passe = "FHUHUHUduzuy uohoihufzoif";
+let mot_de_passe1 = "65oif";
+let mot_de_passe2 = "656duzhufzoif";
+
+function verification(mot) {
+  let tab_mot = mot.split("");
+  let nb_uppercase = 0;
+  let nb_number = 0;
+  if (tab_mot.length < 8) return "mot de passe invalide";
+  for (let i = 0; i < tab_mot.length; i++) {    
+    if (tab_mot[i] === tab_mot[i].toUpperCase()) {
+      nb_uppercase++;
     }
+    if(tab_mot[i] !== ' ')
+      if (!isNaN(Number(tab_mot[i]) )) {
+        nb_number++;
+      }
+    }
+  if(nb_uppercase === 0){
+    return "mot de passe invalide";
+  }else if(nb_number === 0){
+    return "mot de passe invalide";
+  }else{
+    return "mot de passe valide";
   }
-  return "Les mots sont des palindrome";
 }
 
-console.log(arePalindrome("greee", "eeerg"));
-
-
-
+console.log(verification(mot_de_passe));
+console.log(verification(mot_de_passe1));
+console.log(verification(mot_de_passe2));
 
